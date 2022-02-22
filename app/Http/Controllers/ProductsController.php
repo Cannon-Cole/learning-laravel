cd38a4b429b52788123d8c05377f7c050de1015c
Cannon-Cole <colecannonschool@gmail.com>
2022-02-22 00:15:59 -0700

    initial commit

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    public function index()
+    {
+        print_r(route('products'));
+
+        return view('products.index');
+    }
+
+    public function about()
+    {
+        return 'About Us Page';
+    }
+
+    public function show($id)
+    {
+        $data = [
+            'iphone' => 'iPhone',
+            'samsung' => 'Samsung',
+        ];
+
+        return view('products.index', ['products' => $data[$id] ?? 'Product ' . $id . ' does not exist.']);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/products', [ProductsController::class, 'index']);
+
+Route::get('/products/about', [ProductsController::class, 'about']);
+
+Route::get('/products/{name}/{id}', [ProductsController::class, 'show'])->where(['name' => '[a-zA-Z]+', 'id' => '[0-9]+']
+);
+
+Route::get('/products', [ProductsController::class, 'index'])->name('products');
